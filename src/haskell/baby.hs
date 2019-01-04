37f8412952ffd9d417d75b087905f560e4f0bd6b
zunyi <457590303@qq.com>
2019-01-04 19:44:35 +0800

    function

--- src/haskell/baby.hs
+++ src/haskell/baby.hs
@@ -19,3 +19,24 @@ factorial n = n * factorial(n - 1)
 -- Tuple 相加
 addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
 addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+-- 模式匹配
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+capital :: String -> String
+capital "" = "Empty!"
+capital all@(x:xs) = "The first latter of " ++ all ++ " is " ++ [x]
+
+-- 守卫
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+  | weight / height ^ 2 <= 18.5 = "underweight!"
+  | weight / height ^ 2 <= 25.0 = "normal"
+  | weight / height ^ 2 <= 30.0 = "fat!"
+  | otherwise                   = "faaaat!"
