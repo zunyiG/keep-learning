17497dbf8b04a293b23fac2cdc5f5d72f0932a7a
zunyi <457590303@qq.com>
2019-04-12 18:56:54 +0800

    _

--- src/haskell/computer-program.hs
+++ src/haskell/computer-program.hs
@@ -67,13 +67,30 @@ fibonacci n = fibonacci_iter 0 1 n
 -- 1.将现金数 a 换成除第一种硬币以外的其它硬币的数目
 -- 2.将现金数 a - d 换成所有种类的不同方式的数目， 其中 d 是第一种硬币的币值
 
-first_denomination kinds = head [x | (y, x) <- [(1, 1), (2, 5), (3, 10), (4, 25), (5, 50)], y == kinds]
-
 count_change_iter :: Integer -> Integer -> Integer
 count_change_iter amount kinds
   | amount == 0 = 1
   | amount < 0 || kinds == 0 = 0
   | otherwise = (count_change_iter amount (kinds - 1)) + (count_change_iter (amount - (first_denomination kinds)) kinds)
+  where first_denomination kinds = head [x | (y, x) <- [(1, 1), (2, 5), (3, 10), (4, 25), (5, 50)], y == kinds]
 
 count_change :: Integer -> Integer
 count_change amount = count_change_iter amount 5
+
+-- test 1.11
+f1_11_recursion :: Integer -> Integer
+f1_11_recursion x
+  | x < 3 = x
+  | otherwise = (f1_11_recursion (x - 1)) + 2 * (f1_11_recursion (x - 2)) + 3 * (f1_11_recursion (x - 3))
+
+
+
+f1_11_foreach :: Integer -> Integer
+f1_11_foreach n
+  | n < 3 = n
+  | otherwise = f1_11_foreach_iter 2 1 0 3 n
+
+f1_11_foreach_iter :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer
+f1_11_foreach_iter a b c counter n
+  | counter > n = a
+  | otherwise = f1_11_foreach_iter (a + (2 * b) + (3 * c)) a b (counter + 1) n
