b703cb1e95d6064fa2ac9af6f548cc064d4eeeef
zunyi <457590303@qq.com>
2019-10-25 10:37:44 +0800

    _

--- src/nodeschool/webgl/assets/BoilerplateApp/scripts/app.js
+++ src/nodeschool/webgl/assets/BoilerplateApp/scripts/app.js
@@ -5,44 +5,45 @@ var scene=new THREE.Scene(),
     renderer = new THREE.WebGLRenderer(),
     box,
     ground,
+    mesh,
+    loader = new THREE.JSONLoader(),
     controls=null;
 
     function initScene(){
 
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        document.getElementById("webgl-container").appendChild(renderer.domElement);
+      renderer.setSize( window.innerWidth, window.innerHeight );
+      document.getElementById("webgl-container").appendChild(renderer.domElement);
 
-        scene.add(light);
+      scene.add(light);
 
-        camera = new THREE.PerspectiveCamera(
-                35,
-                window.innerWidth / window.innerHeight,
-                1,
-                1000
-            );
-        camera.position.set( 60, 100, 500 );
-        camera.lookAt(new THREE.Vector3(0, 0, 0));
+      camera = new THREE.PerspectiveCamera(
+              20,
+              window.innerWidth / window.innerHeight,
+              0.1,
+              2000
+          );
+      camera.position.set( 0, 900, 600 );
+      camera.lookAt(new THREE.Vector3(0, 75, 0));
 
-        scene.add(camera);
-        var loader = new THREE.JSONLoader(),
-        mesh;
+      scene.add(camera);
 
-        loader.load('../assets/gooseFull.js', function (geometry) {
-                  var gooseMaterial = new THREE.MeshLambertMaterial({
-                  map: THREE.ImageUtils.loadTexture('../assets/goose.jpg')
-              });
+      loader.load('../assets/gooseFull.js', function (geometry) {
+                var gooseMaterial = new THREE.MeshLambertMaterial({
+                map: THREE.ImageUtils.loadTexture('../assets/goose.jpg')
+            });
 
-              mesh = new THREE.Mesh(geometry, gooseMaterial);
-              mesh.scale.set(150, 150, 150);
-              mesh.position.set(0, -150, -200);
+            mesh = new THREE.Mesh(geometry, gooseMaterial);
+            mesh.scale.set(150, 150, 150);
+            mesh.position.set(0, 0, 0);
 
-              scene.add(mesh);
-        });
+            scene.add(mesh);
+      });
 
-        requestAnimationFrame(render);
+      requestAnimationFrame(render);
 
-      };
+    };
     function render() {
+      mesh && (mesh.rotation.y += 0.01)
       renderer.render(scene, camera);
       requestAnimationFrame(render);
     };
